d36772f67f973dd4581dc5265f832e7b1283dd84
LyaShket <dingofleep@gmail.com>
2020-02-26 17:35:43 +0200

    my second commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// burger
+$(window).on("load resize", function(){
+	if ($(window).width() < 768){
+		if (document.querySelector(".menu__button.active"))
+			$('.menu__box,.menu__button').addClass('active');
+			$('body').add('lock');
+	} else {
+		if (document.querySelector(".menu__button.active"))
+			$('.menu__box,.menu__button').removeClass('active');
+			$('body').removeClass('lock');
+  }
+
+});
+$(document).ready(function(){
+	$('.menu__button').click(function(event) {
+		$('.menu__button,.menu__box').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+// ibg
+$(document).ready(function(){
+	let ibg = document.getElementsByClassName("ibg");
+	for (let i = 0; i < ibg.length; i++) {
+  		let item = $(ibg[i]);
+  		let child = item.children("img");
+		let link = child.attr("src");
+		let background = item.css("background-image");
+		item.css("background", `${background} 100% 100% no-repeat, url(${link}) 50% 50% no-repeat`);
+		item.css("background-size", "cover");
+		child.remove();
+	}
+});
+
+// sliders
+$(document).ready(function(){
+    $('.places__slider').slick({
+        slidesToShow: 3,
+		slidesToScroll: 1,
+		centerMode: true,
+		variableWidth: true,
+	});
+});
